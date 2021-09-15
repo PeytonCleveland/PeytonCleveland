3394742644cb5366d45151d6c3bf5bc9d74997ef
Peyton Cleveland <peyton.cleveland.1@gmail.com>
2021-09-15 01:58:15 -0500

    removed tag

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,6 @@ export default function Home() {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://peytoncleveland.com/" />
         <meta property="og:locale" content="en_US" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
